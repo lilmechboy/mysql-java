5696eeefeef4f14c2aef1cd77579e0cf2180a4f9
BriceK <bricekauffman@gmail.com>
2024-11-07 17:08:35 -0800

    week 9 assignment

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -1,13 +1,161 @@
 package projects;
 
 import projects.dao.DbConnection;
+import projects.exception.DbException;
+import projects.service.ProjectService;
+import projects.entity.Project;
+
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+import java.math.BigDecimal;
 
 public class ProjectsApp {
-
+	private Scanner scanner = new Scanner(System.in);
+	private ProjectService projectService = new ProjectService();
+	
+	//@formatter:off
+	private List<String> operations = List.of(
+		"1) Create and populate all tables",
+		"2) Add a project"
+	);
+	// @formatter:on
 
 	public static void main(String[] args) {
-		DbConnection.getConnection();
+		new ProjectsApp().displayMenu();
 
 	}
+	
+	private void displayMenu() {
+		boolean done = false;
+		
+		while(!done) {
+			int operation = getOperation();
+			
+			try {
+			switch(operation) {
+			case -1:
+				done = exitMenu();
+				break;
+				
+			case 1:
+				createTables();
+				break;
+				
+			case 2:
+				addProject();
+				break;
+				
+			default:
+				System.out.println("\n" + operation + " is not valid. Try again.");
+				break;
+			
+			}
+			} catch(Exception e) {
+				System.out.println("\nError: " + e.toString() + " Try again");
+			}
+		} 
+		
+	}
+	
+	private void addProject() {
+		String name = getStringInput("Enter the project name");
+		BigDecimal estimatedHours = getBigDecimalInput("Enter the estimated hours");
+		BigDecimal actualHours = getBigDecimalInput("Enter the actual hours");
+		Integer difficulty = getIntInput("Enter how difficult the project is");
+		String notes = getStringInput("Enter project notes");
+		
+		Project project = new Project();
+		
+		project.setProjectName(name);
+		project.setEstimatedHours(estimatedHours);
+		project.setActualHours(actualHours);
+		project.setDifficulty(difficulty);
+		project.setNotes(notes);
+		
+		Project dbProject = projectService.addProject(project);
+		System.out.println("You added this project: \n" + dbProject);
+		
+		
+	}
 
+	private void createTables() {
+		projectService.createAndPopulateTables();
+		System.out.println("\nTables created and populated!");
+		
+	}
+
+	private boolean exitMenu() {
+		System.out.println("\nExiting the menu.");
+		return true;
+	}
+
+	private int getOperation() {
+		printOperations();
+		Integer op = getIntInput("Enter an operation number (Press Enter to quit)");
+		
+		return Objects.isNull(op) ? -1 : op;
+	}
+
+	private void printOperations() {
+		System.out.println();
+		System.out.println("Here's what you can do:");
+		
+		operations.forEach(op -> System.out.println("	" + op));
+				
+	}
+	
+	private Integer getIntInput(String prompt) {
+		String input = getStringInput(prompt);
+		
+		if(Objects.isNull(input)) {
+			return null;
+		}
+		
+		try {
+			return Integer.parseInt(input);
+		}
+		catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid number.");
+		}
+	}
+	
+	private Double getDoubleInput(String prompt) {
+		String input = getStringInput(prompt);
+		
+		if(Objects.isNull(input)) {
+			return null;
+		}
+		
+		try {
+			return Double.parseDouble(input);
+		}
+		catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid number.");
+		}
+	}
+	
+	private String getStringInput(String prompt) {
+		System.out.println(prompt + ": ");
+		String line = scanner.nextLine();
+		
+		return line.isBlank() ? null : line.trim();
+	}
+	
+	private BigDecimal getBigDecimalInput(String prompt) {
+		Double input = getDoubleInput(prompt);
+		
+		if(Objects.isNull(input)) {
+			return null;
+		}
+		
+		try {
+			return BigDecimal.valueOf(input);
+		}
+		catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid BigDecimal.");
+		}
+	}
+	
 }
---
+++ src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,123 @@
+package projects.service;
+
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+
+import projects.dao.ProjectDao;
+import projects.entity.Project;
+import projects.exception.DbException;
+
+public class ProjectService {
+
+	public ProjectService() {
+	}
+	
+	private static final String SCHEMA_FILE = "project_schema.sql";
+	//private static final String DATA_FILE = "project_data.sql";
+	
+	private ProjectDao projectDao = new ProjectDao();
+	
+	public void createAndPopulateTables() {
+		loadFromFile(SCHEMA_FILE);
+		//loadFromFile(DATA_FILE);
+	}
+
+	
+	private void loadFromFile(String fileName) {
+		
+		String content = readFileContent(fileName);
+		List<String> sqlStatements = convertContentToSqlStatments(content);
+		
+		//sqlStatements.forEach(line -> System.out.println(line));
+		
+		projectDao.executeBatch(sqlStatements);
+	}
+
+
+	private List<String> convertContentToSqlStatments(String content) {
+		content = removeComments(content);
+		content = replaceWhitespaceSequenceWithSingleSpace(content);
+		
+		return extractLinesFromContent(content);
+	}
+
+
+	private List<String> extractLinesFromContent(String content) {
+		List<String> lines = new LinkedList<>();
+		
+		while(!content.isEmpty()) {
+			int semicolon = content.indexOf(";");
+			
+			if (semicolon == -1) {
+				
+				if(!content.isBlank() ) {
+					lines.add(content);
+				}
+				
+				content = "";
+			}
+			
+			else {
+				lines.add(content.substring(0, semicolon).trim());
+				content = content.substring(semicolon + 1);
+				
+			}
+			
+		}
+			
+		return lines;
+	}
+
+
+	private String replaceWhitespaceSequenceWithSingleSpace(String content) {
+		return content.replaceAll("\\s+", " ");
+	}
+
+
+	private String removeComments(String content) {
+		StringBuilder builder = new StringBuilder(content);
+		int commentPosition = 0;
+		
+		while((commentPosition = builder.indexOf("-- ", commentPosition)) != -1) {
+			int eolPosition = builder.indexOf("\n", commentPosition + 1);
+			
+			if(eolPosition == -1) {
+				
+				builder.replace(commentPosition, builder.length(), "");
+			}
+			else {
+				
+				builder.replace(commentPosition, eolPosition + 1, "");
+			}
+		}
+		
+		return builder.toString();
+	}
+
+
+	private String readFileContent(String fileName) {
+		try {
+			Path path = Paths.get(getClass().getClassLoader().getResource(fileName).toURI());
+			return Files.readString(path);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
+	}
+	
+	
+//	public static void main(String[] args) {
+//		new ProjectService().createAndPopulateTables();
+//		
+//	}
+
+
+	public Project addProject(Project project) {
+		
+		return projectDao.insertProject(project);
+	}
+	
+}
